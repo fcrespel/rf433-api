21e0502325edec74c06a9a7064ac4b138748f057
Fabien Crespel <fabien@crespel.net>
2026-02-01 19:38:00 +0100

    feat: add PUT state operation for Chacon 54662
    Also improve response types and codes

--- app/chacon54662/routes.py
+++ app/chacon54662/routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Body, Path, Query, Request
+from pydantic import BaseModel, Field
 
 from .protocol import transmit
 
@@ -7,8 +8,14 @@ __all__ = ["router"]
 router = APIRouter(prefix="/chacon54662", tags=["chacon54662"])
 
 
+class PutStateRequest(BaseModel):
+    word: str = Field(pattern='^[01]{24}$')
+    state: int = Field(ge=0, le=1)
+    repeat: int = Field(ge=1, default=3)
+
+
 @router.get("/state/{stateKey}", summary="Get button state")
-async def get_state(request: Request, stateKey: str = Path(pattern="[a-z0-9-]")):
+async def get_state(request: Request, stateKey: str = Path(pattern="[a-z0-9-]")) -> int:
     """Get the current state for a given stateKey"""
     if stateKey in request.app.state.chacon54662:
         return request.app.state.chacon54662[stateKey]
@@ -16,9 +23,17 @@ async def get_state(request: Request, stateKey: str = Path(pattern="[a-z0-9-]"))
         return 0
 
 
-@router.put("/word", summary="Send 24-bit code word")
-async def put_word(request: Request, word: str = Body(pattern="[01]{24}"), repeat: int = 3,
-                   stateKey: str = Query(default=None, pattern="[a-z0-9-]"), stateValue: int = Query(default=None, ge=0, le=1)):
+@router.put("/state/{stateKey}", summary="Set button state and send 24-bit code word", status_code=204)
+async def put_state(request: Request, stateKey: str = Path(pattern="[a-z0-9-]"), body: PutStateRequest = Body()) -> None:
+    """Set the state for a given stateKey and send a 24-bit code word"""
+    request.app.state.chacon54662[stateKey] = int(body.state)
+    for _ in range(body.repeat):
+        transmit(request.app.state.gpio, int(body.word, 2))
+
+
+@router.put("/word", summary="Send 24-bit code word", status_code=204, deprecated=True)
+async def put_word(request: Request, word: str = Body(pattern="[01]{24}"), repeat: int = Query(default=3, ge=1),
+                   stateKey: str = Query(default=None, pattern="[a-z0-9-]"), stateValue: int = Query(default=None, ge=0, le=1)) -> None:
     """Send a 24-bit code word and optionally save state in a given stateKey"""
     if stateKey is not None and stateValue is not None:
         request.app.state.chacon54662[stateKey] = stateValue
--- app/chacondio10/routes.py
+++ app/chacondio10/routes.py
@@ -8,7 +8,7 @@ router = APIRouter(prefix="/chacondio10", tags=["chacondio10"])
 
 
 @router.get("/{sender}/{button}", summary="Get button state")
-async def get_button(request: Request, sender: int = Path(ge=0, le=67108863), button: int = Path(ge=0, le=15)):
+async def get_button(request: Request, sender: int = Path(ge=0, le=67108863), button: int = Path(ge=0, le=15)) -> int:
     """Get the current state of a button for a given sender"""
     if sender in request.app.state.chacondio10 and button in request.app.state.chacondio10[sender]:
         return request.app.state.chacondio10[sender][button]
@@ -16,8 +16,8 @@ async def get_button(request: Request, sender: int = Path(ge=0, le=67108863), bu
         return 0
 
 
-@router.put("/{sender}/group", summary="Set group state")
-async def put_group(request: Request, sender: int = Path(ge=0, le=67108863), onoff: int = Body(ge=0, le=1), repeat: int = Query(default=5, ge=0)):
+@router.put("/{sender}/group", summary="Set group state", status_code=204)
+async def put_group(request: Request, sender: int = Path(ge=0, le=67108863), onoff: int = Body(ge=0, le=1), repeat: int = Query(default=5, ge=1)) -> None:
     """Set the state of the entire group (all buttons) for a given sender"""
     if not sender in request.app.state.chacondio10:
         request.app.state.chacondio10[sender] = {}
@@ -27,8 +27,8 @@ async def put_group(request: Request, sender: int = Path(ge=0, le=67108863), ono
         transmit(request.app.state.gpio, sender, True, 0, bool(onoff))
 
 
-@router.put("/{sender}/{button}", summary="Set button state")
-async def put_button(request: Request, sender: int = Path(ge=0, le=67108863), button: int = Path(ge=0, le=15), onoff: int = Body(ge=0, le=1), repeat: int = Query(default=5, ge=0)):
+@router.put("/{sender}/{button}", summary="Set button state", status_code=204)
+async def put_button(request: Request, sender: int = Path(ge=0, le=67108863), button: int = Path(ge=0, le=15), onoff: int = Body(ge=0, le=1), repeat: int = Query(default=5, ge=1)) -> None:
     """Set the state of a button for a given sender"""
     if not sender in request.app.state.chacondio10:
         request.app.state.chacondio10[sender] = {}
